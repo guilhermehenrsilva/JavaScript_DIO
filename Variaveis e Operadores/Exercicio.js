a0476f34487a306721f618696e9bf1e8afc4ca10
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2024-04-30 10:03:50 -0300

    First commit

---
+++ Variaveis e Operadores/Exercicio.js
@@ -0,0 +1,20 @@
+let precoCombustivel;
+let distancia ;
+let media;
+const carroKM = 12;
+
+precoCombustivel = 5.50 ;
+distancia = 220 ;
+mediaCombustivel = distancia / carroKM ;
+precoTotalComb = mediaCombustivel * precoCombustivel ;
+
+
+
+
+console.log("A viajem tem", distancia,"KM, o combustivel está" ,"R$",precoCombustivel, 
+"iremos gastar em combustivel uma média de",mediaCombustivel,
+"por KM, totalizando assim, um total gasto de",precoTotalComb)
+
+
+
+
---
+++ Variaveis e Operadores/aula.js
@@ -0,0 +1,12 @@
+//Console e Como Declarar Variáveis
+let variavel = 10 + 10; 
+// let é uma variavel , ou seja consigo mudar ele depois de declarado
+
+//  variavel = 20;
+
+
+// const pi = 3.14; // const é uma constante que depois de declarado um valor , não pode ser alterado
+
+console.log (variavel);
+
+
