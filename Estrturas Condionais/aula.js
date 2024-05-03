058f20c6e0303dbb3b3299d50aeff8fce8c4d2b3
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2024-05-03 09:36:10 -0300

    Atualizado 03/05/2024

--- Estrturas Condionais/aula.js
+++
@@ -1,6 +0,0 @@
-const numero = 9;
-
-const numeroPar = (numero % 2) === 0;
-
-console.log(numeroPar);
-
---
+++ Estruturas Condionais/aula.js
@@ -0,0 +1,25 @@
+// const numero = 0;
+// const numeroPar = (numero % 2) === 0;
+
+// if(numeroPar) {
+//     console.log('Par')
+// }else{
+//     console.log('Impar')
+// }
+
+const numero = 5;
+const numeroDivisivelPor5= (numero % 5) === 0;
+
+if(numero === 0){
+    console.log('numero invalido');
+}else if (numero === 5){
+    console.log('O número é invalidado')
+}else if(numeroDivisivelPor5) {
+    console.log('Sim')
+}else{
+    console.log('Não')
+}
+
+
+
+
